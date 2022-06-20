a248b399123d34b760eaf0299687248ef4a1e1d7
Oussama <0ssama@protonmail.com>
2022-06-20 19:39:32 +0100

    :gear: making crendentials include in profile page

--- src/utils/api/getProfile.ts
+++ src/utils/api/getProfile.ts
@@ -1,6 +1,8 @@
 const getProfile = async (success: () => void, failure: () => void) => {
   try {
-    const res = await fetch("http://localhost:4000/profile");
+    const res = await fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    });
 
     if (res.status === 200) success();
     else failure();
